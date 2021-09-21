fd4e5c40541452f10573a117587b090fc8cba9d0
offsimon <lugers@190089@sus.htl-grieskirchen.at>
2021-09-21 09:28:30 +0200

    construcer added

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,4 +1,11 @@
 public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private int upperLimit;
+
+    public EratosthenesPrimeSieve(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
     @Override
     public boolean isPrime(int p) {
         return false;
